cac8886f79d345f7b734f54bfcea2a0632126abb
gusdn00 <hwgusdn00@naver.com>
2025-12-28 20:09:06 +0900

    xml Viewer 추가

--- src/pages/SheetCompletePage.jsx
+++ src/pages/SheetCompletePage.jsx
@@ -62,8 +62,8 @@ function SheetCompletePage() {
     }
   };
 
-  const handleView = () => {
-  localStorage.setItem('currentSheetUrl', sheetData.result_url);
+  const handleView = (link) => {
+  localStorage.setItem('currentSheetUrl', link);
   window.open('/sheet-viewer', '_blank');
 };
 
@@ -127,7 +127,7 @@ function SheetCompletePage() {
             <div className="btn-group">
               <button
                 className="btn sub-btn"
-                onClick={() => handleView()}
+                onClick={() => handleView(sheet.link)}
               >
                 View
               </button>
